7a63f27508521e4b00811557687b00decf961f4a
Senik <senik@Seniks-MacBook-Pro.local>
2020-11-07 20:50:47 +0100

    :bug: dynamically insert pictures from API call

--- README.md
+++ README.md
@@ -2,5 +2,18 @@
 
 ### TODO:
 
+
 - create frontend skeleton, responsiveness
+- import fonts, decide on layout
+- Navigation:
+dark mode
 - fetch REST API
+
+
+
+Hard tasks:
+*to sort out the component system*, search bar, 
+
+how to not use a framework for components?
+
+how to structure my application - js files etc.
--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,43 @@
-const demo = () => 'Webpack Boilerplate v5.0.1 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+const queryCall = "https://restcountries.eu/rest/v2/all?fields=flag;name;population;region;capital";
 
-// eslint-disable-next-line no-console
-console.log(demo());
+
+// fetch('https://restcountries.eu/rest/v2/all?fields=flag;name;population;region;capital')
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+
+// fetch('https://restcountries.eu/rest/v2/all?fields=flag')
+//     .then(res => res.blob())
+//     .then(blob => {
+//         let img = document.createElement('img');
+//         img.setAttribute("src", blob);
+//         img.width = "100%";
+//         img.height = "100%";
+//         document.getElementsByClassName('flag-div').appendChild('img');
+//     });
+
+
+fetch('https://restcountries.eu/rest/v2/all?fields=flag')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data[0].flag) // Prints result from `response.json()` in getRequest
+        //   Object.keys(data).forEach((key) => {
+        //     const div = document.querySelector('flag-div');
+        //     var img = document.createElement("img");
+        //     img.setAttribute("src", data[0]);
+        //     div.appendChild(img);
+        //     //append ele to parent div
+        //   });
+        let img = document.createElement('img');
+        img.src = data[0].flag;
+        document.getElementById('flag-div').appendChild('img');
+    })
+    .catch(error => console.error(error))
+
+
+/**
+ * TODO: Fetch data from the api call to display picture and other info - random? 
+ * TODO: Do it on load, for 8 countries
+ * TODO: figure out how to do the loading thingy? - scroll down and load or what
+ *
+ */
