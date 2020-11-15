a4e7991312762285e22fb9d7b7bcaa15c77c7388
Senik <senik@Seniks-MacBook-Pro.local>
2020-11-15 15:42:57 +0100

    no repeats flags :fire:

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,65 +1,78 @@
-// fetch flag picture, country, population, region and capital
 /* eslint-disable */
-function createElements() {
+function shuffle(array) {
+  var i = array.length,
+    j = 0,
+    temp;
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function fetchData() {
+  //I have to separate the fetching part
   fetch(
-    'https://restcountries.eu/rest/v2/all?fields=flag;name;population;region;capital',
-  )
+      'https://restcountries.eu/rest/v2/all?fields=flag;name;population;region;capital',
+    )
     .then((res) => res.json())
     .then((data) => {
-      let i = 0;
-      while (i < 8) {
-        let randomNumber = Math.ceil(Math.random() * data.length);
-
-        // **how to check whether we are at the end or not? -> maybe create an array with the numbers and pick one on arndom
-        const previousArray = [];
-        previousArray.push(randomNumber);
-        if (previousArray.includes(randomNumber)) {
-          randomNumber = Math.ceil(Math.random() * data.length);
-        }
-
-        const url = data[randomNumber].flag;
-        const country = data[randomNumber].name;
-        const { population } = data[randomNumber];
-        const { region } = data[randomNumber];
-        const { capital } = data[randomNumber];
-
-        const flexChild = document.createElement('div');
-        flexChild.className = 'main__flags--child';
-        flexChild.innerHTML = `
-                <div class="flag-div"><img class="flag-img" src="${url}" width="100%" height="100%" alt="flag">
-                </div>
-                 <div class="content">
-                 <h1 style="padding: 15px 0;">${country}</h1>
-                <p>Population: ${population}</p>
-                <p>Region: ${region}</p>
-                <p>Capital: ${capital}</p>
-                </div>`;
-        // eslint-disable-next-line no-undef
-        document
-          .querySelector('div.main__flags')
-          .appendChild(flexChild);
-        i += 1;
-      }
+      data = shuffle(data);
+      createElements(data);
     })
     .catch((error) => console.error(error));
 }
 
+function createElements(data){
+  for (obj of data) {
+    const url = obj.flag;
+    const country = obj.name;
+    const population = obj.population;
+    const region = obj.region;
+    const capital = obj.capital;
+    const flexChild = document.createElement('div');
+    flexChild.className = 'main__flags--child';
+    flexChild.innerHTML = `
+          <div class="flag-div"><img class="flag-img" src="${url}" width="100%" height="100%" alt="flag">
+          </div>
+           <div class="content">
+           <h1 style="padding: 15px 0;">${country}</h1>
+          <p>Population: ${population}</p>
+          <p>Region: ${region}</p>
+          <p>Capital: ${capital}</p>
+          </div>`;
+    // eslint-disable-next-line no-undef
+    document
+      .querySelector('div.main__flags')
+      .appendChild(flexChild);
+  }
+}
+
+// }
 // function filterByRegion() {}
 // function search() {}
 
+// function sortByAfrica(data) {
+//   data.sort(function (a, b) {
+//     return parseFloat(a.price) - parseFloat(b.price);
+//   });
+//   console.log(sortArray.sort((a, b) => {
+//     if (a.region < b.region)
+//       return -1;
+//     if (a.region > b.region)
+//       return 1;
+//     return 0;
+//   }));
+// }
+
 // eslint-disable-next-line no-undef
-window.onload = createElements;
-window.onscroll = function () {
-  if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight
-  ) {
-    // if we happen to be at the end, then just disable it
-    createElements();
-  }
-};
+window.onload = fetchData;
+
 /**
  * TODO: don't show the same anymore - parse them into an array and if the array includes then create a new random number
- * TODO: setup Beautify
+ * TODO: what would happen if you search for something that isn't generated yet though
  * TODO: You can infinitively scroll up or down
  */
